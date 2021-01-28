72b9952a4e28ccfeaf14c11a0ebebdabb0281b2d
zerogoon <kckzero@gmail.com>
2021-01-29 00:05:55 +0900

    #1.3 complete

--- src/index.js
+++ src/index.js
@@ -4,10 +4,33 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-    return count;
+number.innerText = 0;
+
+const countModifier = (count = 0, action) => {
+    if (action.type === "ADD") {
+        return count + 1;
+    } else if (action.type === "MINUS") {
+        return count - 1;
+    } else {
+        return count;
+    }
 };
 
 const countStore = createStore(countModifier);
 
-console.log(countStore.getState());
+const onChange = () => {
+    number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+    countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+    countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
